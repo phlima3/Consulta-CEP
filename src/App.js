4b315fa8bf79b7274765c32d4cb1500a13f3a315
phlima3 <psantoslima86@gmail.com>
2022-05-09 14:46:35 -0300

    bug found

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import Carregando from "./Containers/Carregando";
 import Erro from "./Containers/Erro";
 import Resultados from "./Containers/Resultados";
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [resultado, setResultado] = useState({});
 
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<Pesquisa />}/>
-      <Route path="/Carregando" element={<Carregando />}/>
-      <Route path="/Resultados" element={<Resultados result={{ RUA: "São Paulo" }} />}/>
-      <Route path="/Erro" element={<Erro errorMessage="Não foi possível identificar o CEP!" />}/>
-    </Routes>
-  </Router>
-    );
+      <Routes>
+        <Route path="/" element={<Pesquisa setResultado={setResultado} />} />
+        <Route path="/Carregando" element={<Carregando />} />
+        <Route
+          path="/Resultados"
+          element={<Resultados result={{ RUA: "São Paulo" }} />}
+        />
+        <Route
+          path="/Erro"
+          element={<Erro errorMessage="Não foi possível identificar o CEP!" />}
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/Containers/Pesquisa.js
+++ src/Containers/Pesquisa.js
@@ -1,19 +1,45 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import consultarCep from "cep-promise";
 
+function numbersOnly(str) {
+  return str.replace(/[^\d]/g, "");
+}
 
-function Pesquisa() {
-
+function Pesquisa(props) {
+  const setResultado = props.setResultado;
   const [cepNumber, setCepNumber] = useState("");
+  function handleChange(event) {
+    const value = event.target.value;
+    setCepNumber(numbersOnly(value));
+  }
+  function limparState() {
+    setCepNumber("");
+  }
+  function handleSuccsess(dadosCEP) {
+      setResultado(dadosCEP)
+
+  }
+  function handleError(err) {}
+
+  function handleSearch() {
+    consultarCep(cepNumber).than(handleSuccsess).catch(handleError);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Qual CEP você deseja pesquisar?</p>
         <p>Estado atual: {cepNumber}</p>
-        <input style={{ margin: 20 }} />
-        <Link to="/Carregando">
-          <button>CONSULTAR</button>
+        <input
+          value={numbersOnly(cepNumber)}
+          onChange={handleChange}
+          style={{ margin: 20 }}
+        />
+        <button onClick={limparState}>LIMPAR STATE</button>
+
+        <Link to="/Resultados">
+          <button onClick={handleSearch}>CONSULTAR</button>
         </Link>
       </header>
     </div>
